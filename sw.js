fa91281003b05a6885db7a032f8d19dce85a973b
blobbybilb <notablob@outlook.com>
2024-03-02 21:49:15 -0800

    save

--- sw.js
+++ sw.js
@@ -1,37 +1,49 @@
-let currentPageURL
+let lastFetchURL
+
+const channel = new BroadcastChannel("sw-location-updates")
 
 self.addEventListener("fetch", function (event) {
   const url = event.request.url
+  console.log("SW: event.request.url:", url)
 
-  let newReq
-
-  console.log(3333, url, currentPageURL)
-
-  if (url.includes("sw.js")) {
-    const newReq = new Request("/sw.js")
-    console.log(4444, newReq)
-  } else if (
-    url.startsWith("http://") ||
-    url.startsWith("https://") ||
-    url.startsWith("//")
-  ) {
-    newReq = new Request("/" + url)
-  } else if (url.startsWith("/")) {
-    const currentHost = new URL(currentPageURL).origin
-    newReq = new Request(currentHost + url)
-  } else {
-    const url2 = url.startsWith("/") ? url : "/" + url
-    newReq = new Request(currentPageURL + url)
+  if (event.request.mode === "navigate") {
+    if (url.includes("test.html")) return
+    console.log("SW: event.request.mode === navigate")
+    event.respondWith(fetch("/test2.html"))
+    event.preventDefault()
+    channel.postMessage(url)
+    console.log("posted message")
+    // event.preventDefault()
   }
 
-  console.log(newReq)
-  event.respondWith(fetch(newReq))
-  event.preventDefault()
+  //   let newReq
+
+  //   if (
+  //     url.startsWith("http://") ||
+  //     url.startsWith("https://") ||
+  //     url.startsWith("//")
+  //   ) {
+  //     console.log("SW: URL starts with http:// or https:// or //")
+  //     newReq = new Request("/" + url)
+  //   } else if (url.startsWith("/")) {
+  //     console.log("SW: URL starts with /")
+  //     const currentHost = new URL(currentPageURL).origin
+  //     newReq = new Request(currentHost + url)
+  //   } else {
+  //     console.log("SW: URL starts with something else")
+  //     newReq = new Request(url)
+  //   }
+
+  //   console.log("SW: newReq:", newReq)
+
+  //   const fetchCall = fetch(newReq)
+  //   console.log("SW: fetchCall:", fetchCall)
+
+  //   event.respondWith(fetchCall)
+  //   event.preventDefault()
 })
 
-console.log(55555555555, newReq)
-
 self.addEventListener("message", function (event) {
-  currentPageURL = event.data
-  console.log(6666, currentPageURL)
+  lastFetchURL = event.data
+  console.log(6666, lastFetchURL)
 })
